17a40bc08afec2bca1a8e5ba3786d8a5d7d94348
Mateusz <kaczorowski91@gmail.com>
2022-07-13 21:07:47 +0200

    initial commit

---
+++ src/main/java/pl/kaczorowski/quiz/HelloController.java
@@ -0,0 +1,13 @@
+package pl.kaczorowski.quiz;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("hi")
+    public String hello(){
+        return "hello";
+    }
+}
